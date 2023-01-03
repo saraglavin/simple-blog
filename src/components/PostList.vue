11b8826ea1d330f8e2f84ce3d3bf21f235c555c0
Sara Glavin <hej@saraglavin.se>
2023-01-03 15:41:20 +0100

    Added Posts and pages for them.

---
+++ src/components/PostList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="post-list">
+            <div v-for="post in posts" :key="post.id">
+                <SinglePost :post="post" />
+            </div>
+  </div>
+</template>
+
+<script>
+import SinglePost from './SinglePost.vue'
+export default {
+    props: ['posts'],
+    components: { SinglePost },
+    setup(props) {
+        console.log(props.posts);
+
+    }
+}
+</script>
+
+<style>
+
+</style>
