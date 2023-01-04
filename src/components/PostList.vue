fd56341fe75147d1a0b01b6e7064de700fcb6cba
Sara Glavin <hej@saraglavin.se>
2023-01-04 10:30:05 +0100

    Added dummy data in file and prepared for async

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -7,13 +7,17 @@
 </template>
 
 <script>
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core'
 import SinglePost from './SinglePost.vue'
 export default {
     props: ['posts'],
     components: { SinglePost },
     setup(props) {
-        console.log(props.posts);
-
+        onMounted(() => {
+            console.log('comp mounted');
+        })
+        onUnmounted(() =>  console.log('comp unmounted'))
+        onUpdated(() => console.log('Updated'))
     }
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList :posts="posts" />
+    <div v-if="error"> {{ error }} </div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else> 
+      <p>Loading ...</p>
+    </div>
+
   </div>
 </template>
 
@@ -13,12 +20,27 @@ export default {
   name: 'Home',
   components: { PostList },
   setup() {  
-    const posts = ref([
-      { title: 'welcome to the blog', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in imperdiet nisi. Vestibulum eu dui dictum, ornare est non, dignissim dui. Donec at ex nulla. Aenean sem tortor, bibendum ac accumsan vel, elementum vel turpis. Nam auctor egestas pulvinar. Aenean placerat finibus finibus. Praesent dictum velit at purus aliquam, sit amet mattis est molestie. Duis a nulla et mauris luctus feugiat nec cursus odio. Nam elementum vitae est eu porttitor. Nulla vestibulum gravida magna eu ultricies. Nunc posuere tincidunt pellentesque. Sed id condimentum nisl, ultricies viverra orci. Fusce sit amet massa ut nibh feugiat elementum ac id est.', id: 1 },
-      { title: 'top 5 CSS tips', body: 'lorem ipsum', id: 2 },
-    ])
-    
-    return { posts }
+    const posts = ref([])
+    const error = ref(null)
+
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if(!data.ok){
+          throw Error('no data available')
+        }
+
+        posts.value = await data.json()
+
+      }
+      catch (err) {
+        error.value = err.message
+        console.log(error.value);
+      }
+    }
+
+    load()
+    return { posts , error }
   },
 }
 </script>
