19dc51a2969736bea72145950209403bdd4d2c3a
Sara Glavin <hej@saraglavin.se>
2023-01-04 13:57:34 +0100

    Added details and single post

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Details from '../views/Details.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/posts/:id',
+    name: 'Details',
+    component: Details,
+    props: true
   },
   
 ]
