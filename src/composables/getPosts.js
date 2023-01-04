651564d96045e4c024163c1daf906f2e313d27e3
Sara Glavin <hej@saraglavin.se>
2023-01-04 13:01:11 +0100

    Added func getPost and tags

---
+++ src/composables/getPosts.js
@@ -0,0 +1,27 @@
+import { ref } from 'vue'
+
+const getPosts = () => {
+
+    const posts = ref([])
+    const error = ref(null)
+
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if(!data.ok){
+          throw Error('no data available')
+        }
+
+        posts.value = await data.json()
+
+      }
+      catch (err) {
+        error.value = err.message
+        console.log(error.value);
+      }
+    }
+    
+    return { posts, error, load }
+}
+
+export default getPosts
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,32 +13,16 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+
 // component imports
 import PostList from '../components/PostList.vue'
+import getPosts from '../composables/getPosts'
+
 export default {
   name: 'Home',
   components: { PostList },
   setup() {  
-    const posts = ref([])
-    const error = ref(null)
-
-    const load = async () => {
-      try {
-        let data = await fetch('http://localhost:3000/posts')
-        if(!data.ok){
-          throw Error('no data available')
-        }
-
-        posts.value = await data.json()
-
-      }
-      catch (err) {
-        error.value = err.message
-        console.log(error.value);
-      }
-    }
-
+    const { posts, error, load } = getPosts()
     load()
     return { posts , error }
   },
