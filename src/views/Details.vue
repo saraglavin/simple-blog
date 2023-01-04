74dcf39eb4bcc0772acaaa65ad982bd127096be0
Sara Glavin <hej@saraglavin.se>
2023-01-04 16:32:23 +0100

    Added Route to details

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -15,11 +15,15 @@
 <script>
 import getPost from '../composables/getPost'
 import Spinner from '../components/Spinner.vue'
+import { useRoute } from 'vue-router'
+
 export default {
     props: ['id'],
     components: { Spinner },
     setup(props){
-       const { post, error, load } = getPost(props.id) 
+      const route = useRoute()
+
+       const { post, error, load } = getPost(route.params.id) 
 
        load()
 
