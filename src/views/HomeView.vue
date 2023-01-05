89f93259b1f761077b6fee980fecbc8ec6d280e6
Sara Glavin <hej@saraglavin.se>
2023-01-05 10:12:07 +0100

    Added TagCloud, logic of blog completed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <h1>Home</h1>
     <div v-if="error"> {{ error }} </div>
-    <div v-if="posts.length">
+    <div v-if="posts.length" class="layout">
       <PostList :posts="posts" />
       <TagCloud :posts="posts" />
     </div>
@@ -37,5 +37,10 @@ export default {
     max-width: 1200px;
     margin: 0 auto;
     padding: 10px;
+  }
+  .layout {
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    gap: 20px;
   }
 </style>
--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -2,8 +2,9 @@
   <div class="tag">
     <h1>#{{ tag }} </h1>
     <div v-if="error"> {{ error }} </div>
-    <div v-if="filteredPosts.length">
+    <div v-if="posts.length" class="layout">
       <PostList :posts="filteredPosts" />
+      <TagCloud :posts="posts" />
     </div>
     <div v-else> 
       <Spinner />
@@ -15,12 +16,13 @@
 import getPosts from '../composables/getPosts'
 import Spinner from '../components/Spinner.vue'
 import PostList from '../components/PostList.vue'
+import TagCloud from '../components/TagCloud.vue'
 
 import { useRoute } from 'vue-router'
 import { computed } from 'vue'
 
 export default {
-components: { PostList, Spinner },
+components: { PostList, Spinner, TagCloud },
 props: ['tag'],
 setup() {
     const route = useRoute()
