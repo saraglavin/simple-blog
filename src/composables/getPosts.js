b1bcd0fdf562b934830f57c768efae0679ce3a59
Sara Glavin <hej@saraglavin.se>
2023-01-09 10:04:57 +0100

    Changed logic in getPosts to get data fron firestore

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -1,5 +1,5 @@
 import { ref } from 'vue'
-
+import { projectFirestore } from '../firebase/config'
 const getPosts = () => {
 
     const posts = ref([])
@@ -7,17 +7,15 @@ const getPosts = () => {
 
     const load = async () => {
 
-      await new Promise(resolve => {
-        setTimeout(resolve, 500)
-      })
-      
+    
       try {
-        let data = await fetch('http://localhost:3000/posts')
-        if(!data.ok){
-          throw Error('no data available')
-        }
-
-        posts.value = await data.json()
+        const res = await projectFirestore.collection('posts').get()
+        
+        posts.value = res.docs.map(doc => {
+          return {
+            ...doc.data(), id: doc.id
+          }
+        })
 
       }
       catch (err) {
